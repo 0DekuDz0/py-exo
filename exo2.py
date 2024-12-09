25af578b642742f6e91aca7892075819fa37a99b
DekuDz <boudjayanis@gmail.com>
2024-12-09 21:46:11 +0100

    exo2

---
+++ exo2.py
@@ -0,0 +1,8 @@
+x = int(input("temperature in celsius: "))
+if(x <= 0):
+    print("It is freezing")
+elif(x <= 10):
+    print("It is cold")
+elif(x <= 20):
+    print("It is cool")
+print("Stay safe!")
